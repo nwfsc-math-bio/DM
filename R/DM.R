6e26a6c049223166bbe32e1ae2622210dab6c12f
Eli Holmes <eli.holmes@noaa.gov>
2017-03-23 14:53:38 -0700

    added roxygen header
    and removed SelectPrior()

--- R/DM.R
+++ R/DM.R
@@ -1,7 +1,6 @@
+#' @title Shiny app to run DM and VRAP
+#' @aliases DMVRAP
+#' @description This function takes the input from an A & P files, runs DM to estimate SR parameters and saves a .rav file and .RData file. The .rav file can be downloaded or directly used for running the VRAP simulations by clicking the VRAP tab.
 DM=function(){
   shiny:::runApp(system.file('appdir/DMVRAP', package='DM'))
-}
-
-SelectPrior=function(){
-  shiny:::runApp(system.file('appdir/SelectPrior', package='DM'))
 }
